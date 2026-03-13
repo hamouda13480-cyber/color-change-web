b4a3ee5e34ec0ca375245f2654d00a16dc59c5a8
hamouda13480-cyber <hmouda13480@gmial.com>
2026-03-14 01:58:39 +0300

    changing layout of the web

--- main.js
+++ main.js
@@ -6,3 +6,17 @@ function changecolor(){
     let color="rgb("+red+ ","+green+","+blue+")"
     document.getElementById("colorchanger").style.backgroundColor=color
 }
+
+
+
+
+function changecolor(){
+
+    let red=Math.floor(Math.random()*256)
+    let green=Math.floor(Math.random()*256)
+    let blue=Math.floor(Math.random()*256)
+    let color="rgb("+red+ ","+green+","+blue+")"
+    document.getElementById("colorchanger").style.backgroundColor=color
+}
+
+
